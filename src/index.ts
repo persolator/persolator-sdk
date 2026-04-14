af87c1f89a717bd097ec0ce8d913993630ced798
persolator <persolator57@gmail.com>
2026-04-14 18:56:43 +0700

    Export client and types from index.ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export { PersolatorClient, PersolatorError } from "./client";
+export type {
+  PersolatorClientOptions,
+  Market, MarketDetail, MarketStatus, OracleType,
+  OrderBook, OrderBookLevel,
+  Trade,
+  Position, ClosedPosition, PositionSide, PositionStatus,
+  ProtocolStats,
+  LeaderboardEntry,
+  FaucetInfo, FaucetStatus, FaucetClaimResult,
+  VaultInfo, WalletBalance,
+  ScanTransaction, WalletScan, TxType,
+  OpenPositionParams, LaunchMarketParams,
+  ListMarketsOptions, ListPositionsOptions, ListMarketTradesOptions,
+  GetScanOptions, LeaderboardOptions, SortOrder,
+} from "./types";
